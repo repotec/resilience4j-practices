0aea347edf0ddbe3c74d986aee45cbddc924969a
repotec <repotecing@gmail.com>
2022-03-21 23:49:00 +0300

    initial commit

---
+++ src/main/java/com/resilience4j/demo/resilience4jpractices/Resilience4jPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.resilience4j.demo.resilience4jpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jPracticesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resilience4j/demo/resilience4jpractices/Resilience4jPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resilience4j.demo.resilience4jpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jPracticesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
